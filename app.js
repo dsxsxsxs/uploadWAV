fe4d232e5bfa87311de3aee9375c7df38d23865b
dsxs <dsxsxsxs@gmail.com>
2015-05-24 00:38:43 +0800

    upload wav with base64 encoding

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var port=3000;
 var express = require('express');
 var bodyparser = require('body-parser');
+var fs = require('fs');
 var app = express();
 
 app.use(express.static(__dirname + '/assets/'));
-app.use(bodyparser());
+app.use(bodyparser({limit: '50mb', extended: true}));
 app.get('/', function(req, resp){
     resp.sendfile(__dirname+'/assets/index.html');
 });
 app.post('/wav', function(req, resp){
-
-
+    console.log(req.body.wavename);
+    var buf = new Buffer(req.body.blob, 'base64')
+    fs.writeFile(__dirname+"/wave/"+req.body.wavename, buf, function(err) {
+      if(err) {
+        console.error(err);
+      }
+        resp.send({rs: true});
+    });
 });
 
 app.listen(port);
